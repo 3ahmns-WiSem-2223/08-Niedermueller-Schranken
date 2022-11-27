19417a8d6bd30b9cd4989cc2e824836dec10df40
TobiasNiedermueller <114598146+TobiasNiedermueller@users.noreply.github.com>
2022-11-27 17:04:56 +0100

    Add files via upload
    Assets

---
+++ Assets/MyGame/Script/Schranken.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class Schranken : MonoBehaviour
+{
+    [SerializeField]
+    GameObject linksSchranke;
+    [SerializeField]
+    GameObject rechtsSchranke;
+    public Animator anim;
+    public Animator anim2;
+    
+    void Update()
+    {
+        if (!linksSchranke.activeSelf && !rechtsSchranke.activeSelf)
+        {
+            Debug.Log("ja");
+            anim.SetTrigger("los");
+            anim2.SetTrigger("los2");
+        }
+    }
+
+    public void buttonLinks()
+    {
+        if (linksSchranke.activeSelf)
+        {
+            linksSchranke.SetActive(false);
+
+        }
+        else
+        {
+            linksSchranke.SetActive(true);
+        }
+    }
+
+    public void buttonRechts()
+    {
+        if (rechtsSchranke.activeSelf)
+        {
+            rechtsSchranke.SetActive(false);
+
+        }
+        else
+        {
+            rechtsSchranke.SetActive(true);
+        }
+    }
+}
